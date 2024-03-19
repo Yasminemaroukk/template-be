acd035ee06b1e7f4fc76d62a3eabe3ab569aeb42
yessmine marzouk <yasmine.marzouk@avaxia-group.com>
2024-03-19 02:41:56 +0100

    first

---
+++ src/user/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  AutoIncrement,
+  Column,
+  CreatedAt,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'users',
+})
+export class User extends Model<User> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @Column
+  name: string;
+
+  @CreatedAt
+  @Column({ field: 'created_at' })
+  createdAt: Date;
+
+  @UpdatedAt
+  @Column({ field: 'updated_at' })
+  updatedAt: Date;
+}
